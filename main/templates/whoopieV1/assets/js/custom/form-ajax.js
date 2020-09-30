6146aaa99d8d3fbb4a9bb49ed23baf51b181dbe3
rouzbehsbz <polybius1999@gmail.com>
2020-09-30 14:55:47 +0330

    fix profile image

---
+++ main/templates/whoopieV1/assets/js/custom/form-ajax.js
@@ -0,0 +1,91 @@
+$( function () {
+        if(sessionStorage.getItem('reload') == 'true') {
+            showMessage(sessionStorage.getItem('message'));
+            $('.error_info').addClass('success_error')
+            sessionStorage.setItem('reload', 'false');
+        }
+    }
+);
+
+$('.form_ajax').submit(function(event){
+
+    event.preventDefault();
+
+    let fd_object = new FormData();
+    let post_url = $(this).attr("action");
+    let request_method = $(this).attr("method");
+    let form_type = $(this).attr("name");
+    let form_data = getFormData($(this), fd_object);
+
+    let isFileUpload = document.getElementsByClassName('ajax_file');
+    let ajax_options = {
+        url : post_url,
+        method: request_method,
+        dataType : 'json',
+        contentType : false,
+        processData : false,
+    }
+
+    switch(form_type){
+
+        case 'login':
+        {
+
+            let front_validation = loginError();
+            if(!front_validation) return false;
+            break;
+
+        }
+
+    }
+
+    if (Object.keys(form_data).length == 0 && isFileUpload.length > 0) {
+
+        let files = $('.ajax_file')[0].files[0];
+        let file_name = $('.ajax_file').attr('name');
+        form_data.append(file_name, files);
+
+    }
+
+    ajax_options.contentType = false;
+    ajax_options.processData = false;
+    ajax_options.data = form_data;
+
+    $.ajax(ajax_options).done(function(response){
+        if(response.status == 'success'){
+            sessionStorage.setItem('reload', 'true');
+            sessionStorage.setItem('message', response.msg);
+            redirect(response.url);
+        }
+        else if(response.status == 'success-noMsg'){
+            redirect(response.url);
+        }
+        else{
+            showMessage(response)
+        }
+
+    });
+
+});
+
+function getFormData($form, form_data){
+    var unindexed_array = $form.serializeArray();
+
+    $.map(unindexed_array, function(n, i){
+        form_data.append([n['name']], n['value']);
+    });
+
+    return form_data;
+}
+
+function showMessage(text){
+
+    error();
+    $('.error_info p').text(text)
+    return false;
+
+}
+
+function redirect(url) {
+    location.href = url
+}

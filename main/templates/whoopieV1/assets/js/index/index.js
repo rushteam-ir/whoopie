08025527263846bd4fdb067a10e09f55b07cd630
meganoob <kasrahabibbeygi@gmail.com>
2020-09-30 12:54:20 -0700

    creat login and register modal

--- main/templates/whoopieV1/assets/js/index/index.js
+++ main/templates/whoopieV1/assets/js/index/index.js
@@ -1,5 +1,28 @@
-$('#show_AP03').click(function(){
+// open register modal 
 
-    $('#show_AP03').parent().next().fadeToggle();
+
+$('#openRM').click(function () {
+
+    $('#register_modal').fadeIn();
+
+    $('#close_rm').click(function () {
+
+        $('#register_modal').fadeOut();
+
+    });
+
+})
+
+// open login modal 
+
+$('#openLM').click(function () {
+
+    $('#login_modal').fadeIn();
+
+    $('#close_Lm').click(function () {
+
+        $('#login_modal').fadeOut();
+
+    });
 
 })
---
+++ main/templates/whoopieV1/assets/js/layout/layout.js
@@ -0,0 +1,5 @@
+$('#show_AP03').click(function(){
+
+    $('#show_AP03').parent().next().fadeToggle();
+
+})

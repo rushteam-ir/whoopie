b7ef05a7b713b7f3abc086c8c6f2ab557d1a4cb5
meganoob <kasrahabibbeygi@gmail.com>
2020-10-07 10:15:18 -0700

    fix again fucking select

--- main/templates/whoopieV1/assets/js/custom/select.js
+++ main/templates/whoopieV1/assets/js/custom/select.js
@@ -9,7 +9,7 @@ for (let j = 0; j < title.length; j++) {
 
     title.eq(j).click(function () {
 
-        $(this).parent().toggleClass('up_up')
+        $(this).parent().toggleClass('up_up');
         
         let ThisOptionParent = $(this).next();
         let thisOption = $(this).next().children().children();
@@ -49,8 +49,9 @@ title.click(function(){
     $(window).click(function(e){
 
         if(titleClicked.attr('id') !== $(e.target).attr('id')){
-
+            
             titleClicked.next().fadeOut(100);
+            titleClicked.parent().removeClass('up_up');
 
         }
     

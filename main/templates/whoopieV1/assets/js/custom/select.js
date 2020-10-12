13149a7ae2d710629fcc555cb64300b84e31b130
meganoob <kasrahabibbeygi@gmail.com>
2020-10-12 15:44:53 -0700

    fix select id and fix some css bugs

--- main/templates/whoopieV1/assets/js/custom/select.js
+++ main/templates/whoopieV1/assets/js/custom/select.js
@@ -10,7 +10,7 @@ for (let j = 0; j < title.length; j++) {
     title.eq(j).click(function () {
 
         $(this).parent().toggleClass('up_up');
-        
+
         let ThisOptionParent = $(this).next();
         let thisOption = $(this).next().children().children();
 
@@ -28,8 +28,8 @@ for (let j = 0; j < title.length; j++) {
 
             thisOption.eq(i).click(function () {
 
-                thisOption.attr('selected' , false);
-                $(this).attr('selected' , true);
+                thisOption.attr('selected', false);
+                $(this).attr('selected', true);
 
                 $(this).parent().parent().prev().prev().children('p').text($(this).text());
 
@@ -43,20 +43,37 @@ for (let j = 0; j < title.length; j++) {
 }
 
 // when click out of the title of select , close the select
-title.click(function(){
+title.click(function () {
 
     let titleClicked = $(this);
 
-    $(window).click(function(e){
+    $(window).click(function (e) {
 
-        if(titleClicked.attr('id') !== $(e.target).attr('id')){
-            
+        if (titleClicked.attr('id') !== $(e.target).attr('id')) {
+
             titleClicked.next().fadeOut(100);
             titleClicked.parent().removeClass('up_up');
 
         }
-    
+
     });
 
 });
 
+
+// when page load , select who have selected attr , going to be selected :)
+
+let idArray = [];
+
+for (let z = 0; z < $('.select_click').length; z++) {
+
+    let get_id = $('.select_click').eq(z).attr('id');
+
+    idArray.push(get_id);
+    let temp = idArray[z];
+
+    let selected_text = $(`#${temp}`).next().children().children('span[selected]').text();
+    
+    $(`#${temp}`).prev().children('p').text(selected_text);
+
+}

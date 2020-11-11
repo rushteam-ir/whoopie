f7eabaac5621f3c9cca3d4ab47b0d74beaf1a831
rouzbehsbz <polybius1999@gmail.com>
2020-11-11 16:32:18 +0330

    fix search bugs

--- models/ad-schema.js
+++ models/ad-schema.js
@@ -149,10 +149,11 @@ ad_schema.statics = {
                 doc.rate = string_similarity.compareTwoStrings(search_inp.toLowerCase(), index_for_search.toLowerCase());
 
                 if(doc.rate >= 0.03){
-                    doc.rate += doc.author.rep
+                    doc.rate += doc.author.rep;
                     final_list.push(doc);
                 }
                 if(search_inp === ""){
+                    doc.rate = doc.author.rep;
                     final_list.push(doc);
                 }
 
@@ -160,7 +161,7 @@ ad_schema.statics = {
 
         }
 
-        return final_list.sort((a, b)=>{return b - a}).slice((filters.page - 1) * 3, filters.page * 3);
+        return final_list.sort((a, b)=>{return b.rate - a.rate}).slice((filters.page - 1) * 3, filters.page * 3);
 
     }
 
